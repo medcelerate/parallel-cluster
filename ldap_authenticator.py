223838b2af83837be6492151b0911b968a6b1490
djevanclark <djevanclark@Evans-MacBook-Pro-2.local>
2020-03-10 13:31:29 -0400

    updated ldap script

--- ldap_authenticator.py
+++ ldap_authenticator.py
@@ -14,8 +14,8 @@ with open("/efs/opt/hostname") as fp:
 with open("/opt/ltpsecret") as fp:
     user = "(&(objectClass=posixAccount)(uid=%s))" % (sys.argv[1])
     s = fp.read().rstrip()
-    call = subprocess.check_output(['ldapsearch','-x','-H','ldaps://' + host + ":389",'-D',
+    call = subprocess.check_output(" ".join(['export', 'LDAPTLS_REQCERT=allow', '&&' ,'ldapsearch','-x','-H','ldaps://' + host + ":389",'-D',
                         'cn=admin,ou=admin,dc=pcprod,dc=com', '-b', 'dc=pcprod,dc=com',
-                        '-s', 'sub', user, '-w', s, "-o", "ldif-wrap=no"])
+                        '-s', 'sub', user, '-w', s, "-o", "ldif-wrap=no"]), shell=True, executable="/bin/bash")
 
 print("\n".join([ x.split(": ")[1] for x in call.splitlines() if "sshPublicKey" in x]))
