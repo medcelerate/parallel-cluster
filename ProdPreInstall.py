24d471478e0d37426a9474a6d460d796edac8102
djevanclark <djevanclark@Evans-MacBook-Pro-2.local>
2020-04-06 12:40:47 -0400

    changed instance type to ondemand

---
+++ ProdPreInstall.py
@@ -0,0 +1,31 @@
+import os
+import subprocess
+import sys
+
+def install_docker():
+    try:
+        os.makedirs("/efs/opt/docker")
+    except:
+        pass
+
+    rc = subprocess.check_call("sudo amazon-linux-extras install -y docker", shell=True, executable="/bin/bash")
+    if rc != 0:
+        print("Failed at installing docker.")
+        sys.exit(1)
+    rc = subprocess.check_call("sudo systemctl enable docker", shell=True, executable="/bin/bash")
+    if rc != 0:
+        print("Failed at enabling docker daemon.")
+        sys.exit(1)
+
+    rc = subprocess.check_call("sudo systemctl start docker", shell=True, executable="/bin/bash")
+    if rc != 0:
+        print("Failed at starting docker daemon.")
+        sys.exit(1)
+
+    return 0
+
+def main():
+    install_docker()
+
+if __name__ == "__main__":
+    main()
